f6257790f8d7ac41ba7eb2198863beade58b3cd9
hartmama <hartmama98@gmail.com>
2020-01-11 17:00:00 +0100

    initial commit

---
+++ rotarytable.cpp
@@ -0,0 +1,194 @@
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+#if _MSC_VER <= 1200
+#include"objbase.h"
+#include"initguid.h"
+#else
+#define INITGUID
+#include"guiddef.h"
+#undef  INITGUID
+#endif
+#ifdef __cplusplus
+}
+#endif
+
+#include "vciguid.h"
+#include "XatBrds.h"
+#include "stdafx.h"
+#include "objbase.h"
+#include "initguid.h"
+#include "CopError.h"
+#include "copcmd.h"
+#include "Cop.h"
+
+#include "rotarytable.h"
+
+#include <iostream> 
+
+
+
+RotaryTable::RotaryTable(){
+
+	//-----------This Constructor initalizes the can-board, the interface and adding the node to the can-network and prepares the motor for the rotations---------------------
+
+	//Initializing the board; in case of any error, checking the canopen masterapi manual 2.2
+	m_Board_Hdl = 1;
+	m_boardtype = GUID_USB2CANV2_DEVICE;
+	m_boardID = COP_1stBOARD;
+	m_canline = COP_FIRSTLINE;
+	m_res = 0;
+
+
+	//Initializing the interface
+	m_baudtable = COP_k_BAUD_CIA;
+	m_baudrate = COP_k_1000_KB;
+	m_node_no_master = 0;
+	m_hbtime = 0;
+	m_AddFeatures = COP_k_NO_FEATURES;
+	m_res2 = 0;
+
+	//Adding node to can network
+	m_NgOrHb = COP_k_HEARTBEAT;
+	m_GuardHeartbeatTime = 500;
+	m_lifetimefactor = 0;
+	m_node_no_slave = 127;
+	m_res3 = 0;
+
+
+	//Initializsation of board
+	m_res = COP_InitBoard(&m_Board_Hdl, &m_boardtype, &m_boardID, m_canline);
+	std::cout << m_res << std::endl;
+
+	try
+	{
+		if (m_res != BER_k_OK)
+		{ 
+			throw "Initialization of board failed";
+
+		}
+
+	}
+	catch (const char *e)
+	{
+		std::cout << e << std::endl;
+
+	}
+
+	//Initialization of interface
+	m_res2 = COP_InitInterface(m_Board_Hdl, m_baudtable, m_baudrate, m_node_no_master, m_hbtime, m_AddFeatures);
+	std::cout << m_res2 << std::endl;
+
+	try
+	{
+		if (m_res2 != COP_k_OK)
+		{
+			throw "Initialization of interface failed";
+		}
+
+	}
+	catch (const char *e)
+	{
+		std::cout << e << std::endl;
+
+	}
+
+
+	//Add of node to the can network
+	m_res3 = COP_AddNode(m_Board_Hdl, m_node_no_slave, m_NgOrHb, m_GuardHeartbeatTime + 100, m_lifetimefactor);
+	std::cout << m_res3 << std::endl;
+
+	try
+	{
+		if (m_res3 != COP_k_OK)
+		{
+			throw "Add of the node failed";
+
+		}
+
+	}
+	catch (const char *e)
+	{
+		std::cout << e << std::endl;
+	}
+
+	WORD idx_vector[2] = { 0x4000, 0x4150 };
+	BYTE subidx_vector[2] = { 2, 1 };
+	BOOL value_vector[2] = { 1, 1, };
+	SHORT results[2];
+	m_sdo_no = COP_k_DEFAULT_SDO;
+	m_mode = COP_k_NO_BLOCKTRANSFER;
+	m_abortcode = 0;
+
+	for (auto i = 0; i < 2; i++)
+	{
+		results[i] = COP_WriteSDO(m_Board_Hdl, m_node_no_slave, m_sdo_no, m_mode, idx_vector[i], subidx_vector[i], sizeof(value_vector[i]), (PBYTE)&value_vector[i], &m_abortcode);
+		
+		std::cout << results[i] << std::endl;
+		
+		try
+		{
+			if (results[i] != 0)
+			{
+				throw "One of the Initialization steps failed";
+			}
+		}
+		catch (const char *e)
+		{
+			std::cout << e << std::endl;
+		}
+
+	}
+
+}
+
+/*************************Function for setting the velocity of the servo motor in U/min*************************************************/
+void RotaryTable::SetVelocity(BOOL velocity, BOOL v_acc, BOOL v_dec) {
+	m_vel = velocity;
+
+	m_sdo_no = COP_k_DEFAULT_SDO;
+	m_mode = COP_k_NO_BLOCKTRANSFER;
+	
+	//good values for vel_acc and vel_dec is 1000
+	m_abortcode = 0;
+	m_vel_acc_dv = v_acc;
+	m_vel_acc_dt = 5000;
+	m_vel_dec_dv = v_dec;
+	m_vel_dec_dt = 5000;
+
+	/**************Setting the values for acceleration/deceleration*****************/
+	COP_WriteSDO(m_Board_Hdl, m_node_no_slave, m_sdo_no, m_mode, 0x4340, 1, sizeof(m_vel_acc_dv), (PBYTE)&m_vel_acc_dv, &m_abortcode);
+	COP_WriteSDO(m_Board_Hdl, m_node_no_slave, m_sdo_no, m_mode, 0x4341, 1, sizeof(m_vel_acc_dt), (PBYTE)&m_vel_acc_dt, &m_abortcode);
+	COP_WriteSDO(m_Board_Hdl, m_node_no_slave, m_sdo_no, m_mode, 0x4342, 1, sizeof(m_vel_dec_dv), (PBYTE)&m_vel_dec_dv, &m_abortcode);
+	COP_WriteSDO(m_Board_Hdl, m_node_no_slave, m_sdo_no, m_mode, 0x4343, 1, sizeof(m_vel_dec_dt), (PBYTE)&m_vel_dec_dt, &m_abortcode);
+
+	//Writing the corresponding velocity
+	COP_WriteSDO(m_Board_Hdl, m_node_no_slave, m_sdo_no, m_mode, 0x4000, 0x11, sizeof(m_vel), (PBYTE)&m_vel, &m_abortcode);
+
+	
+}
+
+void RotaryTable::StartMovement() {
+
+	m_abortcode = 0;
+	m_mode_motor = 0x74;
+	m_continue = 0x04;
+	//writing the continue param if motor has already moved
+	COP_WriteSDO(m_Board_Hdl, m_node_no_slave, m_sdo_no, m_mode, 0x4000, 1, sizeof(m_continue), (PBYTE)&m_continue, &m_abortcode);
+	//Starting motor
+	COP_WriteSDO(m_Board_Hdl, m_node_no_slave, m_sdo_no, m_mode, 0x4000, 2, sizeof(m_mode_motor), (PBYTE)&m_mode_motor, &m_abortcode);
+
+}
+
+void RotaryTable::StopMovement(void) {
+
+	m_abortcode = 0;
+	m_mode_motor = 0x03;
+	//Stopping motor
+	COP_WriteSDO(m_Board_Hdl, m_node_no_slave, m_sdo_no, m_mode, 0x4000, 1, sizeof(m_mode_motor), (PBYTE)&m_mode_motor, &m_abortcode);
+
+}
+
+RotaryTable::~RotaryTable(){
+}
---
+++ rotarytable.h
@@ -0,0 +1,116 @@
+#ifndef ROTARYTABLE_H
+#define ROTARYTABLE_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+#if _MSC_VER <= 1200
+#include"objbase.h"
+#include"initguid.h"
+#else
+#define INITGUID
+#include"guiddef.h"
+#undef  INITGUID
+#endif
+#ifdef __cplusplus
+}
+#endif
+
+//Needed for type def
+#include "objbase.h"
+#include "Cop.h"
+
+class RotaryTable
+{
+public:
+	/*Constructor and destructor for any instances of the rotarytable class */
+	RotaryTable();
+	~RotaryTable();
+	
+	/*Use this function to set the params for the ramps and the needed velocity in U/min  */
+	void SetVelocity(BOOL velocity, BOOL v_acc, BOOL v_dec);
+
+	/*Functions to start and stop the movement the movement*/
+	void StartMovement(void);
+	void StopMovement(void);
+
+private:
+
+//every m_res variable stores information about a possible error of the corresponding step
+COP_t_HANDLE m_Board_Hdl;
+GUID m_boardtype;
+GUID m_boardID;
+long m_canline;
+SHORT m_res;
+
+
+BYTE m_baudtable;
+BYTE m_baudrate;
+BYTE m_node_no_master;
+BYTE m_node_no_slave;
+WORD m_hbtime;
+DWORD m_AddFeatures;
+SHORT m_res2;
+
+BYTE m_NgOrHb;
+WORD m_GuardHeartbeatTime;
+BYTE m_lifetimefactor;
+SHORT m_res3;
+
+BYTE m_sdo_no;
+BYTE m_mode;
+WORD m_idx;
+BYTE m_subidx;
+DWORD m_buffer_size;
+DWORD m_code;
+SHORT m_res_4;
+
+BOOL m_vel;
+DWORD m_txlen;
+BYTE* m_txdata;
+BOOL m_buffer;
+DWORD m_abortcode;
+SHORT m_res_5;
+BOOL m_mode_motor;
+BOOL m_datareceiver;
+BOOL m_size_datarec;
+BOOL m_status;
+BOOL m_continue;
+
+BOOL m_vel_acc_dv;
+BOOL m_vel_acc_dt;
+BOOL m_vel_dec_dv;
+BOOL m_vel_dec_dt;
+
+
+};
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+//BOOL RotaryTable::GetPos() { return m_currentpos; }
+
+
+
+
+
+
+
+#endif
