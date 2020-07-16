a607a67ffacc2add1a99260de207b698c28712fe
smauch <samuel_mauch@gmx.de>
2020-07-16 21:04:57 +0200

    first pattern running, class rewrite

--- WaitPattern.cpp
+++ WaitPattern.cpp
@@ -18,28 +18,30 @@
 #include <fstream>
 #include <iostream>
 
-WaitPattern::WaitPattern()
+
+WaitPattern::WaitPattern(Gantry* gantry, Tracker* tracker, RotaryTable* rotary)
 {
-	
+	this->gantry = gantry;
+	this->tracker = tracker;
+	this->rotary = rotary;
 }
 
 WaitPattern::~WaitPattern()
 {
-
+	candiesToRotary();
 }
 
-
-
-
-bool WaitPattern::candiesToRotary(Gantry* gantry)
+bool WaitPattern::candiesToRotary()
 {
-	int len = placed_candies.size();
+	rotary->startVelMode(50);
+	int len = placedCandies.size();
 	for (int i=0; i<len; i++ )
 	{
-		gantry->catchStatic(placed_candies[0], DISC_DROP);
-		placed_candies.erase(placed_candies.begin());
+		if (gantry->placeOnTable(placedCandies[0])) {
+			placedCandies.erase(placedCandies.begin());
+		}
 	}
-	if (placed_candies.empty())
+	if (placedCandies.empty())
 	{
 		return true;
 	}
@@ -49,168 +51,151 @@ bool WaitPattern::candiesToRotary(Gantry* gantry)
 	}
 }
 
-
-void RunningCandy::placeCandy(Gantry* gantry, Tracker* tracker, RotaryTable* rotary, Colors maj, Colors min)
+bool WaitPattern::getFromRotary(std::map<std::array<uunit, Gantry::NUM_AMP>, Colors> candiesTo)
 {
-	pos_store.push_back(POS1);
-	pos_store.push_back(POS2);
-	pos_store.push_back(POS3);
-	pos_store.push_back(POS4);
-	pos_store.push_back(POS5);
-	bool catched;
-	int i = 0;
-	//pick up four candies of the same color
-	for (auto it = pos_store.begin(); it < pos_store.end(); it++)
-	{
-		if (i <= 3)
-		{
+	for (auto const& [targetPos, reqCandy] : candiesTo) {
+		int failedTries = 0;
+		Colors targetCandy = reqCandy;
+		while (failedTries < 3) {
 			gantry->prepareCatch();
-			std::this_thread::sleep_for(std::chrono::milliseconds(500));
+			double angVelStart = rotary->getTableAngVel();
 			try
 			{
 				auto start = std::chrono::high_resolution_clock::now();
-				Candy candy = tracker->getCandyOfColor(maj, false);
+				Candy candy = tracker->getCandyOfColor(targetCandy, false);
 				auto end = std::chrono::high_resolution_clock::now();
 				auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-				double angVel = rotary->getTableAngVel();
-				candy.setCurrentPosition(candy.getCurrentPosition().rotate(angVel / 1000 * 180 / M_PI * elapsed));
-				catched = gantry->catchRotary(angVel, candy.getCurrentPosition().getAngle(), candy.getCurrentPosition().getR(), *it);
-
+				candy.setCurrentPosition(candy.getCurrentPosition().rotate(angVelStart / 1000 * 180 / M_PI * elapsed));
+				double ang = candy.getCurrentPosition().getAngle();
+				double radiusFact = candy.getCurrentPosition().getR() / tracker->getOuterR();
+				bool catchSucess = gantry->catchRotary(ang, angVelStart, radiusFact, targetPos);
+				if (!catchSucess) {
+					failedTries++;
+					gantry->prepareCatch();
+				}
+				else {
+					this->placedCandies.push_back(targetPos);
+					break;
+				}
 			}
 			catch (const NoCandyException&)
 			{
-				std::this_thread::sleep_for(std::chrono::milliseconds(500));
-				//make something
+				failedTries++;
+				//Prevent running out of candies
+				if (!gantry->fillTable(targetCandy)) {
+					targetCandy = ANY;
+				}
 			}
-			if (catched)
-			{
-				placed_candies.push_back(*it);
-				i += 1;
-
-			}
-
 		}
-		else
-		{
-			try
-			{
-				auto start = std::chrono::high_resolution_clock::now();
-				Candy candy = tracker->getCandyOfColor(min, false);
-				auto end = std::chrono::high_resolution_clock::now();
-				auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-				double angVel = rotary->getTableAngVel();
-				candy.setCurrentPosition(candy.getCurrentPosition().rotate(angVel / 1000 * 180 / M_PI * elapsed));
-				catched = gantry->catchRotary(angVel, candy.getCurrentPosition().getAngle(), candy.getCurrentPosition().getR(), *it);
-
-			}
-			catch (const NoCandyException&)
-			{
-				std::this_thread::sleep_for(std::chrono::milliseconds(500));
-				//make something
-			}
-			if (catched)
-			{
-				placed_candies.push_back(*it);
-
-			}
-
-		}
-	}
-}
-
-bool RunningCandy::runCandy(Gantry* gantry)
-{
-	for (int i = 0; i < 3; i++)
-	{
-		//assuming the unique candy is placed in pos 4
-		gantry->catchStatic(placed_candies[3-i], INIT_POS);
-		gantry->catchStatic(placed_candies[4-i], placed_candies[4-i]);
-		gantry->catchStatic(INIT_POS, placed_candies[4-i]);
 	}
 	return true;
 }
 
-void CandyTower::buildTower(Gantry* gantry, Tracker* tracker, RotaryTable* rotary)
+
+
+bool RunningCandy::runCandy()
 {
-	pos_store.push_back(POS1);
-	pos_store.push_back(POS2);
-	pos_store.push_back(POS3);
-	pos_store.push_back(POS4);
-	pos_store.push_back(POS5);
-
-	bool catched;
-	int i = 0;
-	for (auto it = pos_store.begin(); it < pos_store.end(); it++)
+	int len = placedCandies.size();
+	len = len - 1;
+	gantry->catchStatic(placedCandies[len], INIT_POS);
+	for (int i = 1; i <= len; i++)
 	{
-		try
-		{
-			Colors nextCandy = static_cast<Colors>(i);
-			auto start = std::chrono::high_resolution_clock::now();
-			Candy candy = tracker->getCandyOfColor(nextCandy, false);
-			auto end = std::chrono::high_resolution_clock::now();
-			auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-			double angVel = rotary->getTableAngVel();
-			candy.setCurrentPosition(candy.getCurrentPosition().rotate(angVel / 1000 * 180 / M_PI * elapsed));
-			catched = gantry->catchRotary(angVel, candy.getCurrentPosition().getAngle(), candy.getCurrentPosition().getR(), *it);
+		gantry->catchStatic(placedCandies[len - i], placedCandies[len - i + 1]);
+	}
+	gantry->catchStatic(INIT_POS,placedCandies[0]);
+	return true;
+}
 
-		}
-		catch (const NoCandyException&)
-		{
-			std::this_thread::sleep_for(std::chrono::milliseconds(500));
-			//make something
-		}
-		if (catched)
-		{
-			placed_candies.push_back(*it);
-			i += 1;
-
-		}
+void RunningCandy::calcTargetPos(std::vector<Colors> candies)
+{
+	std::array<uunit, Gantry::NUM_AMP> currPos = INIT_POS;
+	for (auto const& it : candies) {
+		currPos[1] = currPos[1] + 1.5 * CANDY_SIZE[0];
+		candiesTo.insert({ currPos, it });
 	}
 }
 
-void CandyCircle::buildCircle(Gantry* gantry, Tracker* tracker, RotaryTable* rotary)
-{
-	pos_store.push_back(POS1);
-	pos_store.push_back(POS2);
-	pos_store.push_back(POS3);
-	pos_store.push_back(POS4);
-	pos_store.push_back(POS5);
-	pos_store.push_back(POS6);
-	pos_store.push_back(POS7);
-	pos_store.push_back(POS8);
-	bool catched;
-	int i = 0;
-	for (auto it = pos_store.begin(); it < pos_store.end(); it++)
-	{
-		try
-		{
-			if (i >= 7)
-			{
-				i = i - 7;
-			}
-			Colors nextCandy = static_cast<Colors>(i);
-			auto start = std::chrono::high_resolution_clock::now();
-			Candy candy = tracker->getCandyOfColor(nextCandy, false);
-			auto end = std::chrono::high_resolution_clock::now();
-			auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-			double angVel = rotary->getTableAngVel();
-			candy.setCurrentPosition(candy.getCurrentPosition().rotate(angVel / 1000 * 180 / M_PI * elapsed));
-			catched = gantry->catchRotary(angVel, candy.getCurrentPosition().getAngle(), candy.getCurrentPosition().getR(), *it);
-
-		}
-		catch (const NoCandyException&)
-		{
-			std::this_thread::sleep_for(std::chrono::milliseconds(500));
-			//make something
-		}
-		if (catched)
-		{
-			placed_candies.push_back(*it);
-			i += 1;
-
-		}
-	}
-
-
-}
+//void CandyTower::buildTower(Gantry* gantry, Tracker* tracker, RotaryTable* rotary)
+//{
+//	pos_store.push_back(POS1);
+//	pos_store.push_back(POS2);
+//	pos_store.push_back(POS3);
+//	pos_store.push_back(POS4);
+//	pos_store.push_back(POS5);
+//
+//	bool catched;
+//	int i = 0;
+//	for (auto it = pos_store.begin(); it < pos_store.end(); it++)
+//	{
+//		try
+//		{
+//			Colors nextCandy = static_cast<Colors>(i);
+//			auto start = std::chrono::high_resolution_clock::now();
+//			Candy candy = tracker->getCandyOfColor(nextCandy, false);
+//			auto end = std::chrono::high_resolution_clock::now();
+//			auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+//			double angVel = rotary->getTableAngVel();
+//			candy.setCurrentPosition(candy.getCurrentPosition().rotate(angVel / 1000 * 180 / M_PI * elapsed));
+//			catched = gantry->catchRotary(angVel, candy.getCurrentPosition().getAngle(), candy.getCurrentPosition().getR(), *it);
+//
+//		}
+//		catch (const NoCandyException&)
+//		{
+//			std::this_thread::sleep_for(std::chrono::milliseconds(500));
+//			//make something
+//		}
+//		if (catched)
+//		{
+//			placed_candies.push_back(*it);
+//			i += 1;
+//
+//		}
+//	}
+//}
+//
+//void CandyCircle::buildCircle(Gantry* gantry, Tracker* tracker, RotaryTable* rotary)
+//{
+//	pos_store.push_back(POS1);
+//	pos_store.push_back(POS2);
+//	pos_store.push_back(POS3);
+//	pos_store.push_back(POS4);
+//	pos_store.push_back(POS5);
+//	pos_store.push_back(POS6);
+//	pos_store.push_back(POS7);
+//	pos_store.push_back(POS8);
+//	bool catched;
+//	int i = 0;
+//	for (auto it = pos_store.begin(); it < pos_store.end(); it++)
+//	{
+//		try
+//		{
+//			if (i >= 7)
+//			{
+//				i = i - 7;
+//			}
+//			Colors nextCandy = static_cast<Colors>(i);
+//			auto start = std::chrono::high_resolution_clock::now();
+//			Candy candy = tracker->getCandyOfColor(nextCandy, false);
+//			auto end = std::chrono::high_resolution_clock::now();
+//			auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+//			double angVel = rotary->getTableAngVel();
+//			candy.setCurrentPosition(candy.getCurrentPosition().rotate(angVel / 1000 * 180 / M_PI * elapsed));
+//			catched = gantry->catchRotary(angVel, candy.getCurrentPosition().getAngle(), candy.getCurrentPosition().getR(), *it);
+//
+//		}
+//		catch (const NoCandyException&)
+//		{
+//			std::this_thread::sleep_for(std::chrono::milliseconds(500));
+//			//make something
+//		}
+//		if (catched)
+//		{
+//			placed_candies.push_back(*it);
+//			i += 1;
+//
+//		}
+//	}
+//
+//
+//}
 
--- WaitPattern.h
+++ WaitPattern.h
@@ -18,72 +18,75 @@
 #include <vector>
 #include <fstream>
 #include <iostream>
+
+
 class WaitPattern
 {
 
 public:
-	WaitPattern();
+	WaitPattern(Gantry* gantry, Tracker* tracker, RotaryTable* rotary);
 	~WaitPattern(); 
-	bool candiesToRotary(Gantry* gantry);
-
+	
 protected:
-	std::vector<std::array<uunit, Gantry::NUM_AMP>> placed_candies;
-	std::vector<std::array<uunit,Gantry::NUM_AMP>> pos_store;
-	const std::array<uunit, 3> INIT_POS = { 500,46350,0 };
-	const std::array<uunit, 3> CANDY_SIZE = { 55,42, 10 };
-	const std::array<uunit, 3> DISC_DROP = { 60000, 46350, 25000 };
+	Gantry* gantry;
+	Tracker* tracker;
+	RotaryTable* rotary;
+
+	bool candiesToRotary();
+	bool getFromRotary(std::map<std::array<uunit, Gantry::NUM_AMP>, Colors> candiesTo);
+	std::vector<std::array<uunit, Gantry::NUM_AMP>> placedCandies;
+	std::array<uunit, 3> INIT_POS = Gantry::WAIT_PAT_BASE;
+	std::array<uunit, 3> CANDY_SIZE = Gantry::CANDY_SIZE;
 };
 
 class RunningCandy: public WaitPattern
 {
 public:
-	void placeCandy(Gantry* gantry, Tracker* tracker, RotaryTable* rotary, Colors maj, Colors min);
-	bool runCandy(Gantry* gantry);
-
-
-
-private:
-	const std::array<uunit, 3> POS1 = { INIT_POS[0] - 2 * CANDY_SIZE[0] , INIT_POS[1], 0 };
-	const std::array<uunit, 3> POS2 = { POS1[0] ,POS1[1] + CANDY_SIZE[1] , 0 };
-	const std::array<uunit, 3> POS3 = { POS2[0], POS2[1] + CANDY_SIZE[1] ,0 };
-	const std::array<uunit, 3> POS4 = { POS3[0], POS3[1] + CANDY_SIZE[1], 0 };
-	const std::array<uunit, 3> POS5 = { POS4[0], POS4[1] + CANDY_SIZE[1], 0 };
-
-
-};
-
-class CandyTower : public WaitPattern
-{
-public:
-	void buildTower(Gantry* gantry, Tracker* tracker, RotaryTable* rotary);
-
+	RunningCandy(std::vector<Colors> candies, Gantry* gantry, Tracker* tracker, RotaryTable* rotary) : WaitPattern(gantry, tracker, rotary) {
+		calcTargetPos(candies);
+		getFromRotary(candiesTo);
+	};
+	bool runCandy();
 
 private:
-	const std::array<uunit, 3> POS1 = { INIT_POS[0], INIT_POS[1], INIT_POS[2] + CANDY_SIZE[2] };
-	const std::array<uunit, 3> POS2 = { INIT_POS[0], INIT_POS[1], POS1[2] + CANDY_SIZE[2] };
-	const std::array<uunit, 3> POS3 = { INIT_POS[0], INIT_POS[1], POS2[2] + CANDY_SIZE[2] };
-	const std::array<uunit, 3> POS4 = { INIT_POS[0], INIT_POS[1], POS3[2] + CANDY_SIZE[2] };
-	const std::array<uunit, 3> POS5 = { INIT_POS[0], INIT_POS[1], POS4[2] + CANDY_SIZE[2] };
-
+	void calcTargetPos(std::vector<Colors> candies);
+	std::map<std::array<uunit, Gantry::NUM_AMP>, Colors> candiesTo;
 };
 
-class CandyCircle : public WaitPattern
-{
-public:
-	void buildCircle(Gantry* gantry, Tracker* tracker, RotaryTable * rotary);
-
-private:
-	const std::array<uunit, 3> POS1 = {INIT_POS[0] + 2 * CANDY_SIZE[0], INIT_POS[1], 0};
-	const std::array<uunit, 3> POS2 = {POS1[0] - CANDY_SIZE[0], POS1[1] + CANDY_SIZE[1],0};
-	const std::array<uunit, 3> POS3 = {INIT_POS[0], INIT_POS[1] + 2 * CANDY_SIZE[1], 0};
-	const std::array<uunit, 3> POS4 = {POS3[0] - CANDY_SIZE[0], POS3[1] - CANDY_SIZE[1], 0};
-	const std::array<uunit, 3> POS5 = {INIT_POS[0] - 2 * CANDY_SIZE[0], INIT_POS[1],0};
-	const std::array<uunit, 3> POS6 = {POS5[0] + CANDY_SIZE[0],POS5[1] - CANDY_SIZE[1],0 };
-	const std::array<uunit, 3> POS7 = {INIT_POS[0], INIT_POS[1] - 2 * CANDY_SIZE[1],0};
-	const std::array<uunit, 3> POS8 = { POS7[0] + CANDY_SIZE[0], POS7[1] + CANDY_SIZE[1],0 };
 
 
-};
+//class CandyTower : public WaitPattern
+//{
+//public:
+//	void buildTower(Gantry* gantry, Tracker* tracker, RotaryTable* rotary);
+//
+//
+//private:
+//	const std::array<uunit, 3> POS1 = { INIT_POS[0], INIT_POS[1], INIT_POS[2] + CANDY_SIZE[2] };
+//	const std::array<uunit, 3> POS2 = { INIT_POS[0], INIT_POS[1], POS1[2] + CANDY_SIZE[2] };
+//	const std::array<uunit, 3> POS3 = { INIT_POS[0], INIT_POS[1], POS2[2] + CANDY_SIZE[2] };
+//	const std::array<uunit, 3> POS4 = { INIT_POS[0], INIT_POS[1], POS3[2] + CANDY_SIZE[2] };
+//	const std::array<uunit, 3> POS5 = { INIT_POS[0], INIT_POS[1], POS4[2] + CANDY_SIZE[2] };
+//
+//};
+//
+//class CandyCircle : public WaitPattern
+//{
+//public:
+//	void buildCircle(Gantry* gantry, Tracker* tracker, RotaryTable * rotary);
+//
+//private:
+//	const std::array<uunit, 3> POS1 = {INIT_POS[0] + 2 * CANDY_SIZE[0], INIT_POS[1], 0};
+//	const std::array<uunit, 3> POS2 = {POS1[0] - CANDY_SIZE[0], POS1[1] + CANDY_SIZE[1],0};
+//	const std::array<uunit, 3> POS3 = {INIT_POS[0], INIT_POS[1] + 2 * CANDY_SIZE[1], 0};
+//	const std::array<uunit, 3> POS4 = {POS3[0] - CANDY_SIZE[0], POS3[1] - CANDY_SIZE[1], 0};
+//	const std::array<uunit, 3> POS5 = {INIT_POS[0] - 2 * CANDY_SIZE[0], INIT_POS[1],0};
+//	const std::array<uunit, 3> POS6 = {POS5[0] + CANDY_SIZE[0],POS5[1] - CANDY_SIZE[1],0 };
+//	const std::array<uunit, 3> POS7 = {INIT_POS[0], INIT_POS[1] - 2 * CANDY_SIZE[1],0};
+//	const std::array<uunit, 3> POS8 = { POS7[0] + CANDY_SIZE[0], POS7[1] + CANDY_SIZE[1],0 };
+//
+//
+//};
 
 
 
